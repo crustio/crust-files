5adf1e90672d8b5e872c7b66da7d5e0b6c8097cb
eericxu <2681350846@qq.com>
2021-12-06 15:24:22 +0800

    fix load locales json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '@decooio/crust-fonts/style.css'
 import {AppProps} from 'next/app'
 import i18next from "i18next";
 import I18NextHttpBackend from "i18next-http-backend";
-import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+// import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import {initReactI18next} from "react-i18next";
 import React, {useEffect, useMemo, useState} from "react";
 import {Container, Dimmer, Loader} from "semantic-ui-react";
@@ -22,13 +22,14 @@ function initI18n() {
   const [init, setInit] = useState(false)
   useEffect(() => {
     i18next.use(new I18NextHttpBackend())
-      .use(I18nextBrowserLanguageDetector)
+      // .use(I18nextBrowserLanguageDetector)
       .use(initReactI18next)
       .init({
         backend: {
           loadPath: '/locales/{{lng}}.json'
         },
-        fallbackLng: 'zh-CN',
+        lng: 'en',
+        fallbackLng: 'en',
         interpolation: {
           escapeValue: false
         }
