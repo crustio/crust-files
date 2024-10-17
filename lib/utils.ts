809d8a1bc6f4020d05d5c1a64a95f6aa38a9bb35
eericxu <2681350846@qq.com>
2024-10-17 17:31:41 +0800

    fix msg for walletconnet

--- lib/utils.ts
+++ lib/utils.ts
@@ -112,7 +112,7 @@ export const getErrorMsg = (error: any, def = 'Network Error'): string => {
     else if (typeof error?.msg == 'string') msg = error?.msg
     else if (typeof error?.message == 'string') msg = error?.message
     // replace
-    if (msg.includes('User denied') || msg.includes('user rejected transaction'))
+    if (msg.includes('User denied') || msg.includes('user rejected transaction') || msg.includes('User rejected'))
       return 'You declined the action in your wallet.'
     if (msg.includes('transaction failed')) return 'Transaction failed'
     return msg
