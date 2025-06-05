bd54001d81139f879796bd97ece5a05fbc939419
eericxu <2681350846@qq.com>
2025-06-05 17:19:22 +0800

    add error log

--- lib/hooks/useInputFile.ts
+++ lib/hooks/useInputFile.ts
@@ -2,6 +2,7 @@ import { Dispatch, MutableRefObject, SetStateAction, useCallback, useContext, us
 import { useTranslation } from "react-i18next";
 import { AppContext } from "../AppContext";
 import { DirFile, FileInfo } from "../wallet/types";
+import _ from "lodash";
 
 export interface WrapInputFile {
     file?: FileInfo,
@@ -22,61 +23,71 @@ export default function useInputFile(): WrapInputFile {
     const inputRef = useRef<HTMLInputElement>(null);
     const _clickUploadFile = useCallback((dir = false) => {
         if (!inputRef.current) return;
-        // eslint-disable-next-line
-        // @ts-ignore
-        // eslint-disable-next-line
-        inputRef.current.webkitdirectory = dir;
-        // eslint-disable-next-line
-        inputRef.current.multiple = dir;
+        if(dir){
+            // eslint-disable-next-line
+            // @ts-ignore
+            // eslint-disable-next-line
+            inputRef.current.webkitdirectory = dir;
+            // eslint-disable-next-line
+            inputRef.current.multiple = dir;
+        }
         inputRef.current.click();
     }, [inputRef]);
     const _onClickUpFile = useCallback(() => _clickUploadFile(false), [_clickUploadFile]);
     const _onClickUpFolder = useCallback(() => _clickUploadFile(true), [_clickUploadFile]);
     const _onInputFile = useCallback<FunInputFile>((e) => {
-        const files = e.target.files;
-
-        if (!files) return;
-
-        if (files.length > 2000) {
-            alert.alert({
-                title: t('Upload'),
-                msg: t('Please do not upload more than 2000 files'),
-                type: 'error'
-            })
-            return;
-        }
-
-        if (files.length === 0) {
-            alert.alert({
-                title: t('Upload'),
-                msg: t('Please select non-empty folder'),
-                type: 'error'
-            })
-            return;
-        }
-
-        // eslint-disable-next-line
-        // @ts-ignore
-        // eslint-disable-next-line
-        const isDirectory = e.target.webkitdirectory;
-
-        if (!isDirectory) {
-            setFile({ file: files[0] });
-        } else if (files.length >= 1) {
-            const dirFiles: DirFile[] = [];
-            for (let index = 0; index < files.length; index++) {
-                // console.info('f:', files[index]);
-                dirFiles.push(files[index] as DirFile);
+        try {
+            
+            const files = e.target.files;
+            if (!files) return;
+            const size = _.size(files)
+            if (size > 2000) {
+                alert.alert({
+                    title: t('Upload'),
+                    msg: t('Please do not upload more than 2000 files'),
+                    type: 'error'
+                })
+                return;
             }
-
-            console.info(dirFiles);
-
-            const [dir] = dirFiles[0].webkitRelativePath.split('/');
-
-            setFile({ files: dirFiles, dir });
+    
+            if (size === 0) {
+                alert.alert({
+                    title: t('Upload'),
+                    msg: t('Please select non-empty folder'),
+                    type: 'error'
+                })
+                return;
+            }
+    
+            // eslint-disable-next-line
+            // @ts-ignore
+            // eslint-disable-next-line
+            const isDirectory = e.target.webkitdirectory;
+    
+            if (!isDirectory) {
+                setFile({ file: files[0] });
+            } else if (files.length >= 1) {
+                const dirFiles: DirFile[] = [];
+                for (let index = 0; index < files.length; index++) {
+                    // console.info('f:', files[index]);
+                    dirFiles.push(files[index] as DirFile);
+                }
+    
+                console.info(dirFiles);
+    
+                const [dir] = dirFiles[0].webkitRelativePath.split('/');
+    
+                setFile({ files: dirFiles, dir });
+            }
+    
+            e.target.value = '';
+        } catch (error) {
+            alert.alert({
+                title: t('Upload'),
+                msg: 'Select file error ' + JSON.stringify(error),
+                type: 'error'
+            })
         }
-
-        e.target.value = '';
     }, [setFile, alert, t]);
     return useMemo(() => ({
         file,
