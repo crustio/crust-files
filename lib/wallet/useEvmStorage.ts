e0355eb238148d7f27b0af8629d75ef260072db2
eericxu <2681350846@qq.com>
2025-06-06 11:18:45 +0800

    fix network.chainId

--- lib/wallet/useEvmStorage.ts
+++ lib/wallet/useEvmStorage.ts
@@ -6,7 +6,7 @@ import { EVMStorageABI, EVMStorageContract } from "./config";
 export function useEvmStorage() {
   const provider = useWeb3Provider();
   return useMemo(() => {
-    if (!provider) return undefined;
+    if (!provider || !provider.network) return undefined;
     const address = EVMStorageContract[provider.network.chainId];
     if (!address) return undefined;
     return ContractFactory.getContract(address, EVMStorageABI, provider.getSigner());
